19405c55ce0e68272efffd2ee849cb30bf37f93c
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2025-05-02 00:33:33 +0300

    added solution for sort binary tree by level

---
+++ SortBinaryTreeByLevel/solution.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
+def tree_by_levels(node):
+    if not node:
+        return []
+    queue = [node]
+    result = []
+    while queue:
+        current = queue.pop(0)
+        result.append(current.value)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return result
